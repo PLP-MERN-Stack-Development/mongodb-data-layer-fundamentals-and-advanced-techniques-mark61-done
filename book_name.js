08499a64823899867769af38cad5413cdea73168
mark61-done <markoolando558@gmail.com>
2025-10-06 13:38:31 +0300

    first commit

--- book_name.js
+++ book_name.js
@@ -3,120 +3,120 @@ const { book } = require("./models/books");
 
 async function main() {
   await connectDB();
-  // await book.deleteMany({});
+  await book.deleteMany({});
 
-  //   await book.createIndexes({title:1});
-  //       console.log('title index created');
+    await book.createIndexes({title:1});
+        console.log('title index created');
 
-  //       await book.createIndexes({author:1,published_year:-1});
-  //       console.log('compound index created');
+        await book.createIndexes({author:1,published_year:-1});
+        console.log('compound index created');
 
         await book.find({author:'George Orwell'}).explain('executionStats');
         console.log('query explained');
 
-  // await book.insertMany([
-  //   {
-  //     title: "To Kill a Mockingbird",
-  //     author: "Harper Lee",
-  //     genre: "Fiction",
-  //     published_year: 1960,
-  //     price: 12.99,
-  //     in_stock: true,
-  //     pages: 336,
-  //     publisher: "J. B. Lippincott & Co.",
-  //   },
-  //   {
-  //     title: "1984",
-  //     author: "George Orwell",
-  //     genre: "Dystopian",
-  //     published_year: 1949,
-  //     price: 10.99,
-  //     in_stock: true,
-  //     pages: 328,
-  //     publisher: "Secker & Warburg",
-  //   },
-  //   {
-  //     title: "The Great Gatsby",
-  //     author: "F. Scott Fitzgerald",
-  //     genre: "Fiction",
-  //     published_year: 1925,
-  //     price: 9.99,
-  //     in_stock: true,
-  //     pages: 180,
-  //     publisher: "Charles Scribner's Sons",
-  //   },
-  //   {
-  //     title: "Brave New World",
-  //     author: "Aldous Huxley",
-  //     genre: "Dystopian",
-  //     published_year: 2010,
-  //     price: 11.5,
-  //     in_stock: false,
-  //     pages: 311,
-  //     publisher: "Chatto & Windus",
-  //   },
-  //   {
-  //     title: "The Hobbit",
-  //     author: "J.R.R. Tolkien",
-  //     genre: "Fantasy",
-  //     published_year: 1937,
-  //     price: 14.99,
-  //     in_stock: true,
-  //     pages: 310,
-  //     publisher: "George Allen & Unwin",
-  //   },
-  //   {
-  //     title: "The Catcher in the Rye",
-  //     author: "J.D. Salinger",
-  //     genre: "Fiction",
-  //     published_year: 1951,
-  //     price: 8.99,
-  //     in_stock: true,
-  //     pages: 224,
-  //     publisher: "Little, Brown and Company",
-  //   },
-  //   {
-  //     title: "Pride and Prejudice",
-  //     author: "Jane Austen",
-  //     genre: "Romance",
-  //     published_year: 2010,
-  //     price: 7.99,
-  //     in_stock: true,
-  //     pages: 432,
-  //     publisher: "T. Egerton, Whitehall",
-  //   },
-  //   {
-  //     title: "The Lord of the Rings",
-  //     author: "J.R.R. Tolkien",
-  //     genre: "Fantasy",
-  //     published_year: 2021,
-  //     price: 19.99,
-  //     in_stock: true,
-  //     pages: 1178,
-  //     publisher: "Allen & Unwin",
-  //   },
-  //   {
-  //     title: "Animal Farm",
-  //     author: "George Orwell",
-  //     genre: "Political Satire",
-  //     published_year: 1945,
-  //     price: 8.5,
-  //     in_stock: false,
-  //     pages: 112,
-  //     publisher: "Secker & Warburg",
-  //   },
-  //   {
-  //     title: "The Alchemist",
-  //     author: "Paulo Coelho",
-  //     genre: "Fiction",
-  //     published_year: 2022,
-  //     price: 10.99,
-  //     in_stock: true,
-  //     pages: 197,
-  //     publisher: "HarperOne",
-  //   },
-  // ]);
-  // console.log("Books inserted");
-  // await mongoose.disconnect();
+  await book.insertMany([
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      genre: "Fiction",
+      published_year: 1960,
+      price: 12.99,
+      in_stock: true,
+      pages: 336,
+      publisher: "J. B. Lippincott & Co.",
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      genre: "Dystopian",
+      published_year: 1949,
+      price: 10.99,
+      in_stock: true,
+      pages: 328,
+      publisher: "Secker & Warburg",
+    },
+    {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      genre: "Fiction",
+      published_year: 1925,
+      price: 9.99,
+      in_stock: true,
+      pages: 180,
+      publisher: "Charles Scribner's Sons",
+    },
+    {
+      title: "Brave New World",
+      author: "Aldous Huxley",
+      genre: "Dystopian",
+      published_year: 2010,
+      price: 11.5,
+      in_stock: false,
+      pages: 311,
+      publisher: "Chatto & Windus",
+    },
+    {
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien",
+      genre: "Fantasy",
+      published_year: 1937,
+      price: 14.99,
+      in_stock: true,
+      pages: 310,
+      publisher: "George Allen & Unwin",
+    },
+    {
+      title: "The Catcher in the Rye",
+      author: "J.D. Salinger",
+      genre: "Fiction",
+      published_year: 1951,
+      price: 8.99,
+      in_stock: true,
+      pages: 224,
+      publisher: "Little, Brown and Company",
+    },
+    {
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      genre: "Romance",
+      published_year: 2010,
+      price: 7.99,
+      in_stock: true,
+      pages: 432,
+      publisher: "T. Egerton, Whitehall",
+    },
+    {
+      title: "The Lord of the Rings",
+      author: "J.R.R. Tolkien",
+      genre: "Fantasy",
+      published_year: 2021,
+      price: 19.99,
+      in_stock: true,
+      pages: 1178,
+      publisher: "Allen & Unwin",
+    },
+    {
+      title: "Animal Farm",
+      author: "George Orwell",
+      genre: "Political Satire",
+      published_year: 1945,
+      price: 8.5,
+      in_stock: false,
+      pages: 112,
+      publisher: "Secker & Warburg",
+    },
+    {
+      title: "The Alchemist",
+      author: "Paulo Coelho",
+      genre: "Fiction",
+      published_year: 2022,
+      price: 10.99,
+      in_stock: true,
+      pages: 197,
+      publisher: "HarperOne",
+    },
+  ]);
+  console.log("Books inserted");
+  await mongoose.disconnect();
 }
 main();
--- crud.js
+++ crud.js
@@ -4,85 +4,85 @@ const { book } = require("./models/books");
 async function main(){
     await connectDB();
     //CREATE
-    // await book.create({
-    //         title: 'Wuthering Heights',
-    //         author: 'Emily Brontë',
-    //         genre: 'Gothic Fiction',
-    //         published_year: 1847,
-    //         price: 9.99,
-    //         in_stock: true,
-    //         pages: 342,
-    //         publisher: 'Thomas Cautley Newby'
-    // })
-    // console.log('book created')
+    await book.create({
+            title: 'Wuthering Heights',
+            author: 'Emily Brontë',
+            genre: 'Gothic Fiction',
+            published_year: 1847,
+            price: 9.99,
+            in_stock: true,
+            pages: 342,
+            publisher: 'Thomas Cautley Newby'
+    })
+    console.log('book created')
 
-    //READ
-    // const books = await book.find().select('genre');
-    //  const author = await book.find().select('author');
-    // console.log('all books:',books,author);
+    READ
+    const books = await book.find().select('genre');
+     const author = await book.find().select('author');
+    console.log('all books:',books,author);
 
-    // await book.updateOne({title:'1984'},{price:8.99});
-    // console.log('book updated');
+    await book.updateOne({title:'1984'},{price:8.99});
+    console.log('book updated');
 
-    // await book.deleteOne({title:'Wuthering Heights'});
-    // console.log('book deleted');
+    await book.deleteOne({title:'Wuthering Heights'});
+    console.log('book deleted');
 
-    //Filtering books
-//    const latestBooks = await book.find({
-//         in_stock:true,
-//         published_year:{$gt:2010}
-//     },
-//     {title:1,
-//         author:1,
-//         price:1,
-//         _id:0
-//     });
-//     console.log('found:',latestBooks);
+    // Filtering books
+   const latestBooks = await book.find({
+        in_stock:true,
+        published_year:{$gt:2010}
+    },
+    {title:1,
+        author:1,
+        price:1,
+        _id:0
+    });
+    console.log('found:',latestBooks);
      
-         //sorting books by price
-    //  const ascendingOrder = await book.find().sort({price:1});
-    //     console.log('ascending order:',ascendingOrder);
+        //  sorting books by price
+     const ascendingOrder = await book.find().sort({price:1});
+        console.log('ascending order:',ascendingOrder);
 
-        //     const descendingOrder = await book.find().sort({price:-1});
-        // console.log('descending order:',descendingOrder);
+            const descendingOrder = await book.find().sort({price:-1});
+        console.log('descending order:',descendingOrder);
 
-        //Pagination
-        // const booksPerPage = 5;
-        // const currentPage = 1;
-        // const paginatedBooks = await book.find()
-        // .skip((currentPage - 1) * booksPerPage)
-        // .limit(booksPerPage);
-        // console.log(`Books on page ${currentPage}:`, paginatedBooks);
+        // Pagination
+        const booksPerPage = 5;
+        const currentPage = 1;
+        const paginatedBooks = await book.find()
+        .skip((currentPage - 1) * booksPerPage)
+        .limit(booksPerPage);
+        console.log(`Books on page ${currentPage}:`, paginatedBooks);
 
-        //aggregation
-        // const avgPrice = await book.aggregate([
-        //     {$group:{
-        //         _id:'$genre',
-        //         averagePrice:{$avg:'$price'}}},
-        // ])
-        // console.log('books by genre:',avgPrice);
+        // aggregation
+        const avgPrice = await book.aggregate([
+            {$group:{
+                _id:'$genre',
+                averagePrice:{$avg:'$price'}}},
+        ])
+        console.log('books by genre:',avgPrice);
 
-        // const authorWithMostBooks = await book.aggregate([
-        //     {$group:{
-        //         _id:'$author',
-        //         bookCount:{$sum:1}}},
-        //         {$sort:{bookCount:-1}},
-        //         {$limit:1}
-        // ])
-        // console.log('author with most books:',authorWithMostBooks);
+        const authorWithMostBooks = await book.aggregate([
+            {$group:{
+                _id:'$author',
+                bookCount:{$sum:1}}},
+                {$sort:{bookCount:-1}},
+                {$limit:1}
+        ])
+        console.log('author with most books:',authorWithMostBooks);
 
-        // const booksPublishedPerDecade = await book.aggregate([
-        //     {$project:{
-        //         decade:{$multiply:[{$floor:{$divide:['$published_year',10]}},10]},
+        const booksPublishedPerDecade = await book.aggregate([
+            {$project:{
+                decade:{$multiply:[{$floor:{$divide:['$published_year',10]}},10]},
 
-        //     }},
-        //     {$group:{
-        //         _id:'$decade',
-        //         bookCount:{$sum:1}
-        //     }},
-        //     {$sort:{_id:1}}
-        // ]);
-        // console.log('books published per decade:',booksPublishedPerDecade);
+            }},
+            {$group:{
+                _id:'$decade',
+                bookCount:{$sum:1}
+            }},
+            {$sort:{_id:1}}
+        ]);
+        console.log('books published per decade:',booksPublishedPerDecade);
 
         //indexing
         await book.createIndexes({title:1});
